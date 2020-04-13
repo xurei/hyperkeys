6688849580217b866f7ab2de04493311bddd2e4b
Olivier Bourdoux <olivier.bourdoux@gmail.com>
2020-04-13 21:03:16 +0200

    Use msys for OSTYPE

--- src/hyperkeys-extensions/switch-window/build.sh
+++ src/hyperkeys-extensions/switch-window/build.sh
@@ -5,6 +5,6 @@ BASEPATH=$(realpath $(dirname $0))
 
 echo "OS type: $OSTYPE"
 
-if [[ "$OSTYPE" == "win32" ]]; then
+if [[ "$OSTYPE" == "win32" || "$OSTYPE" == "msys" ]]; then
 	g++ $BASEPATH/win32/foregroundwin.cpp
 fi
