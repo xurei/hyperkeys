574e7c1e80bc6466c03850b15500de0d181b9f46
Olivier Bourdoux <olivier.bourdoux@gmail.com>
2020-04-14 15:24:19 +0200

    Run Command - shortened command

--- src/hyperkeys-extensions/run-command/index.js
+++ src/hyperkeys-extensions/run-command/index.js
@@ -1,10 +1,12 @@
+const path = require('path');
+
 module.exports = {
     actions: [
         require('./action-run-command'),
     ],
     metadata: {
         name: 'RUN_COMMAND',
-        title: (config) => config && config.command ? `Run '${config.command}'` : 'Run command',
+        title: (config) => config && config.command ? `Run '${path.basename(config.command)}'` : 'Run command',
         description: 'Run a command line instruction',
         actions: {
             RUN_COMMAND: {title: 'Run command'},
