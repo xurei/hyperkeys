4e00b7c9a754e0fd476c1616fffdd3be4572005f
xurei <olivier.bourdoux@gmail.com>
2021-02-06 21:42:52 +0100

    Removed useless libs

--- scripts/package-linux.sh
+++ scripts/package-linux.sh
@@ -11,7 +11,8 @@ echo "Purging useless libs..."
 rm -rf $BASEPATH/dist_packages/linux-unpacked/swiftshader || true
 rm -rf $BASEPATH/dist_packages/linux-unpacked/libGLESv2.so || true
 rm -rf $BASEPATH/dist_packages/linux-unpacked/libEGL2.so || true
-rm -rf $BASEPATH/dist_packages/linux-unpacked/chrome-sandbox || true
+rm -rf $BASEPATH/dist_packages/linux-unpacked/libvulkan.so || true
+rm -rf $BASEPATH/dist_packages/linux-unpacked/libvk_swiftshader.so || true
 
 # Create AppImage
 node_modules/.bin/electron-builder --prepackaged=dist_packages/linux-unpacked --linux --p always
