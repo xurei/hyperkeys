a0be181ddca3053c95167f75c0e8a40387fbb15b
xurei <olivier.bourdoux@gmail.com>
2021-11-24 00:54:25 +0100

    Keep "useless" libs (they are not that useless actually)

--- scripts/package-linux.sh
+++ scripts/package-linux.sh
@@ -7,12 +7,6 @@ echo $BASEPATH
 node_modules/.bin/electron-builder --dir --linux
 
 # Remove unneeded libs
-echo "Purging useless libs..."
-rm -rf $BASEPATH/dist_packages/linux-unpacked/swiftshader || true
-rm -rf $BASEPATH/dist_packages/linux-unpacked/libGLESv2.so || true
-rm -rf $BASEPATH/dist_packages/linux-unpacked/libEGL2.so || true
-rm -rf $BASEPATH/dist_packages/linux-unpacked/libvulkan.so || true
-rm -rf $BASEPATH/dist_packages/linux-unpacked/libvk_swiftshader.so || true
 
 # Create AppImage
 node_modules/.bin/electron-builder --prepackaged=dist_packages/linux-unpacked --linux --p always
