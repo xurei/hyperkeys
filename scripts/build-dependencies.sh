7ecc5738df3d5f33fd82617928e34aed6771eb54
xurei <olivier.bourdoux@gmail.com>
2022-03-24 10:28:55 +0100

    ⬆ Dependencies update (security)

--- scripts/build-dependencies.sh
+++ scripts/build-dependencies.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -e
 
-BASEPATH=$(realpath $(dirname $0))
+BASEPATH=$(realpath "$(dirname $0)")
 
 # Build production package.json
 node $BASEPATH/build_package_json.js
@@ -14,7 +14,7 @@ cd $BASEPATH/../build
 npm install
 
 # Node rebuild
-npm rebuild --runtime=electron --target=10.3.1 --disturl=https://atom.io/download/atom-shell --abi=$(../node_modules/.bin/electron -a)
+npm rebuild --runtime=electron --target=10.3.1 --disturl=https://atom.io/download/atom-shell --abi="$(../node_modules/.bin/electron -a)"
 
 # Patching dependency files to use local binaries
 patch -N node_modules/active-win/lib/linux.js ../patches/active-win.patch
