9d69592c9c252046820713da43928c461a6d820d
xurei <xureilab@gmail.com>
2024-10-26 01:59:15 +0200

    💄 <Popup> : using more modern CSS (aka flexbox)

--- src/webapp/react-popup.js
+++ src/webapp/react-popup.js
@@ -36,13 +36,14 @@ class Popup extends React.Component {
         const props = this.props;
 		
         return (
-            <div style={{position: 'fixed', left: 0,right: 0,top: 0,bottom: 0, background: 'rgba(0,0,0,0.2)', zIndex: 1000, display: (props.visible ? 'block':'none')}}>
-                <div style={{position: 'absolute', width: '100%', height: '100%', maxWidth: props.maxWidth, maxHeight: props.maxHeight, background: '#111111', top: '50%', left: '50%', transform: 'translate(-50%,-50%)', boxShadow: '#000 0 0 25px', overflow: 'hidden'}}>
-                    <div style={{height: '2.6em', position: 'fixed', width: '100%', background: '#222', padding: '0 8px'}}>
-                        <span style={{position: 'absolute', display: 'inline-block', height: 'inherit', right: '10px', fontSize: '1.8em', color: '#fff', textDecoration: 'none', cursor: 'pointer'}} onClick={props.onClose}>&times;</span>
-                        <h4>{props.title}</h4>
+            <div style={{position: 'fixed', flexDirection: 'column', alignItems: 'center', justifyContent: 'start', left: 0,right: 0,top: 0,bottom: 0, background: 'rgba(0,0,0,0.2)', zIndex: 1000, display: (props.visible ? 'flex':'none')}}>
+                <div style={{height: '25vh'}}></div>
+                <div style={{position: 'relative', width: '100%', maxWidth: props.maxWidth, background: '#111111', boxShadow: '#000 0 0 25px', overflow: 'hidden'}}>
+                    <div style={{height: '2.6em', width: '100%', background: '#222', padding: '11px 8px'}}>
+                        <span style={{position: 'absolute', display: 'inline-block', height: 'inherit', top: 0, right: 0, fontSize: '1.8em', color: '#fff', textDecoration: 'none', cursor: 'pointer', padding: '0 10px 1px 10px'}} onClick={props.onClose}>&times;</span>
+                        <h4 style={{margin: 0}}>{props.title}</h4>
                     </div>
-                    <div style={{position: 'absolute', top: '2.6em', bottom: '0', width: '100%', overflow: 'auto', padding: '8px'}}>
+                    <div style={{width: '100%', padding: '8px 8px 54px 8px'}}>
                         {props.children}
                     </div>
                 </div>
