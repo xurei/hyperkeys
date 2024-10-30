1ee9eeeab032a87d3f4849098f0255419e109b40
xurei <xureilab@gmail.com>
2024-10-30 11:48:37 +0100

    🏗️ Use `createRoot` instead of `ReactDOM.render`

--- src/webapp/webapp.js
+++ src/webapp/webapp.js
@@ -1,5 +1,5 @@
 import React from 'react'; //eslint-disable-line no-unused-vars
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import MainView from './react-view-main';
 import store from './store';
@@ -31,7 +31,7 @@ function initApp() {
         });
 		
         store.dispatch(setMetadatas(metadatas));
-        global.postMessage({ action: 'request_macros'}, '*');
+        ipc.send('request_macros');
     });
     ipc.on('latest_version', (event, release) => {
         console.log('Latest version:', release.tag_name);
@@ -41,29 +41,30 @@ function initApp() {
     ipc.on('macros', (event, arg) => {
         console.log('Got macros', arg);
         store.dispatch(setMacros(arg));
-		
-        //Rendering the UI
-        setTimeout(() => {
-            ReactDOM.render(
-                <Provider store={store}>
-                    <MainView/>
-                </Provider>,
-                document.getElementById('content'),
-            );
-			
-            //TODO fadeout of the loading view
-            var loadingView = document.getElementById('loading-view');
-            loadingView.style.display = 'none';
-        }, 200);
     });
-    global.postMessage({ action: 'request_metadatas'}, '*');
+    ipc.send('request_metadatas');
+		
+    //Rendering the UI
+    setTimeout(() => {
+        const container = document.getElementById('content');
+        const root = createRoot(container);
+        root.render((
+            <Provider store={store}>
+                <MainView/>
+            </Provider>
+        ));
+  
+        //TODO fadeout of the loading view
+        const loadingView = document.getElementById('loading-view');
+        loadingView.style.display = 'none';
+    }, 200);
 }
 
 if( document.readyState !== 'loading' ) {
-    console.log( 'document is already ready' );
+    console.log('document is already ready');
     initApp();
 }
 else {
-    console.log( 'document not ready, waiting for it to load...' );
+    console.log('document not ready, waiting for it to load...');
     document.addEventListener('DOMContentLoaded', initApp);
 }
